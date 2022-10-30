002b5a98fa4e8eaac63c133e4ea7790c6fc38ed9
Pedro Santos <103754512+pedrosantos111599@users.noreply.github.com>
2022-10-30 20:18:53 -0300

    Implementa testes de cadastro em novo projeto

---
+++ backend/src/Controllers/CadastroController.cs
@@ -0,0 +1,24 @@
+﻿using Gestão_Financeira.Models;
+using Gestão_Financeira.Repositorio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gestão_Financeira.Controllers
+{
+    public class CadastroController : Controller
+    {
+        private readonly IUsuarioRepositorio _usuarioRepositorio;
+        public CadastroController(IUsuarioRepositorio usuarioRepositorio)
+        {
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Criar(UserModel usuario)  
+        {
+            _usuarioRepositorio.Adicionar(usuario);
+            return RedirectToAction("Index");
+        }
+    }
+}
