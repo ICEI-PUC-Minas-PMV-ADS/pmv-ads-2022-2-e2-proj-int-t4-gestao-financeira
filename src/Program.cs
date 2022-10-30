a10a4b61e40943e6ec103aed8d9c27a8c5301671
Ottoni Soares <83013553+ottonisoares@users.noreply.github.com>
2022-10-30 00:42:05 -0300

    ASP.NET
    Criação do projeto em ASP.NET e inicialização do sistema de login e cadastro

--- src/Program.cs
+++ src/Program.cs
@@ -1,12 +1,37 @@
-ï»¿using System;
+using Gestão_Financeira.Data;
+using Gestão_Financeira.Repositorio;
+using Microsoft.EntityFrameworkCore;
 
-namespace src
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DataBase");
+builder.Services.AddEntityFrameworkSqlServer().AddDbContext<BancoContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddScoped<IUsuarioRepositorio, UsuarioRepositorio>();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
