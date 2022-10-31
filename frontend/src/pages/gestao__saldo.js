a2d148c7f26893c8bceba70f8ccb1164eed6ba61
ALEXSANDRA DA GRACA SILVA <alexsandra.graca@sme.prefeitura.sp.gov.br>
2022-10-30 22:40:28 -0300

    configurando Corretamente os Retornos da API

--- frontend/src/pages/gestao__saldo.js
+++ frontend/src/pages/gestao__saldo.js
@@ -1,24 +1,36 @@
+import { CommunicateAPI } from "../services/CommunicatesAPI";
+
 export const getaoSaldo = () => {
-    return (`    
+  const { novoSaldo, historicoTransacao } = new CommunicateAPI(
+    "/api/Transacao/transacao-deposito"
+  ).get();
+
+  return `    
         <div class="container__gestao__saldo" >
             <div class="container__apresenta__saldo" >
                 <div class="container__saldo" >
                     <p class="label__valor" >Saldo</p>
-                    <h3 id="saldo__valor" >R$ - 1.000</h3>
+                    <h3 id="saldo__valor" >R$ ${
+                      novoSaldo ? novoSaldo : "0.000,00"
+                    }</h3>
                 </div>
                 <div class="container__transacoes">
                     <div class="container__valor" >
                         <h2 class="flexa flexa__cima" >↑</h2>
                         <div>
                             <p class="label__valor" >Valor Entrada</p>
-                            <h3 id="valor__entrada" class="valor__entrada" >R$ 1.000</h3>
+                            <h3 id="valor__entrada" class="valor__entrada" >R$ R$ ${
+                              novoSaldo ? novoSaldo : "0.000,00"
+                            }/h3>
                         </div>
                     </div>
                     <div class="container__valor" >
                         <h2 class="flexa flexa__baixo">↓</h2>
                         <div>
                             <p class="label__valor" >Valor Saída</p>
-                            <h3 id="valor__saida" class="valor__saida" >R$ 2.000</h3>
+                            <h3 id="valor__saida" class="valor__saida" >R$ R$ ${
+                              novoSaldo ? novoSaldo : "0.000,00"
+                            }</h3>
                         </div>
                     </div>
                 </div>
@@ -27,7 +39,7 @@ export const getaoSaldo = () => {
                 <div class="container__inputs__inserir__valor">
                     <div class="container__inserir__valor">
                         <label class="label__inserir__valor" for="valor" >Insira o valor</label>
-                        <input value="" id="input__valor" class="input__inserir__valor" type="number" name="valor" placeholder="R$ 00,00" >
+                        <input id="input__valor" class="input__inserir__valor" type="text" name="valor" placeholder="R$ 00,00" >
                     </div>
                     <select id="select__tipo__transacao" class="select__tipo__transacao" >
                         <option value="" disabled selected>Tipo de Transação</option>
@@ -38,5 +50,5 @@ export const getaoSaldo = () => {
                 <button id="button__inserir__valor" class="button__inserir__valor">+</button>
             </div>
         </div>
-    `);
+    `;
 };
--- frontend/src/services/CommunicatesAPI.js
+++ frontend/src/services/CommunicatesAPI.js
@@ -1,117 +1,97 @@
 export class CommunicateAPI {
+  #url;
+  #idUser;
 
-    #url;
-    #idUser;
+  constructor(endPoint, idUser) {
+    this.#url = `http://localhost/${endPoint}`;
+    this.#idUser = idUser;
+  }
 
-    constructor(endPoint, idUser) {
-        this.#url = `http://API/${endPoint}`;
-        this.#idUser = idUser;
+  async get() {
+    const options = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    async get() {
+    try {
+      fetch(`${this.#url}/${this.#idUser}`, options)
+        .then((value) => {
+          return value;
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
-        const options = {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        };
-
-        try {
-          
-            fetch(`${this.#url}/${this.#idUser}`, options)
-            .then((value) => {
-
-                return value;
-
-            })
-            .catch((error) => {
-                throw new Error(error);
-            });
-
-        } catch (error) {
-            throw new Error(error);
-        };
+  post(value) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(value),
     };
 
-    post(value) {
+    try {
+      fetch(`${this.#url}`, options)
+        .then((value) => {
+          return value;
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(value),
-        };
-
-        try {
-          
-            fetch(`${this.#url}`, options)
-            .then((value) => {
-
-                return value;
-
-            })
-            .catch((error) => {
-                throw new Error(error);
-            });
-
-        } catch (error) {
-            throw new Error(error);
-        };
+  delete(value) {
+    const options = {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    delete(value) {
-        const options = {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        };
+    try {
+      fetch(`${this.#url}/${this.#idUser}`, options)
+        .then((value) => {
+          return value;
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
 
-        try {
-          
-            fetch(`${this.#url}/${this.#idUser}`, options)
-            .then((value) => {
-
-                return value;
-
-            })
-            .catch((error) => {
-                throw new Error(error);
-            });
-
-        } catch (error) {
-            throw new Error(error);
-        };
+  put(value) {
+    const options = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "Access-Control-Allow-Origin": "*",
+      },
+      body: JSON.stringify(value),
     };
 
-    put(value) {
-        const options = {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify(value),
-        };
-
-        try {
-          
-            fetch(`${this.#url}/${this.#idUser}`, options)
-            .then((value) => {
-
-                return value;
-
-            })
-            .catch((error) => {
-                throw new Error(error);
-            });
-
-        } catch (error) {
-            throw new Error(error);
-        };
-    };
-
-};
+    try {
+      fetch(`${this.#url}/${this.#idUser}`, options)
+        .then((value) => {
+          return value;
+        })
+        .catch((error) => {
+          throw new Error(error);
+        });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+}
