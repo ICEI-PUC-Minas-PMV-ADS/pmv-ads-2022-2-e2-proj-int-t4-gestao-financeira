d3be7179d323bd8af585f8f9b164d39641bb8da1
ALEXSANDRA DA GRACA SILVA <alexsandra.graca@sme.prefeitura.sp.gov.br>
2022-11-13 17:52:51 -0300

    Menu Lateral

--- frontend/src/components/menu/index.js
+++ frontend/src/components/menu/index.js
@@ -1,4 +1,57 @@
-export const Menu = () => {
+import { linksRedirect } from "./links__redirect/index.js";
+
+const closedMenu = () => {
+  let containerItemsList = document.getElementById(
+    "container__items__list__menu__side"
+  );
+
+  containerItemsList.style.animation = "normal animationSideMenuClose 2s";
+
+  setTimeout(() => {
+    let containerPrincipal = document.getElementById("container__principal");
+    containerPrincipal.removeChild(containerItemsList);
+    containerPrincipal.prepend(menuHamburguer());
+  }, 1800);
+};
+
+const menuLateral = () => {
+  let containerItemsList = document.createElement("ul");
+  containerItemsList.className = "container__items__list__menu__side";
+  containerItemsList.id = "container__items__list__menu__side";
+
+  let containerCloseButton = document.createElement("div");
+  containerCloseButton.className = "container__close__button";
+
+  let closeButton = document.createElement("h2");
+  closeButton.innerText = "X";
+  closeButton.className = "close__button";
+  closeButton.addEventListener("click", closedMenu);
+
+  containerCloseButton.append(closeButton);
+
+  containerItemsList.prepend(containerCloseButton);
+
+  linksRedirect.forEach((links) => {
+    let containerItems = document.createElement("li");
+    containerItems.className = "container__items__menu__side";
+
+    let items = document.createElement("p");
+    items.className = "items__menu__side";
+
+    containerItems.addEventListener("click", () => {
+      window.location.href = links.url;
+    });
+
+    containerItemsList.appendChild(containerItems);
+    containerItems.appendChild(items);
+
+    items.innerText = links.displayName;
+  });
+
+  return containerItemsList;
+};
+
+export const menuHamburguer = () => {
   let lineOne = document.createElement("div");
   let lineTwo = document.createElement("div");
   let lineThre = document.createElement("div");
@@ -13,6 +66,12 @@ export const Menu = () => {
   containerMenu.appendChild(lineOne);
   containerMenu.appendChild(lineTwo);
   containerMenu.appendChild(lineThre);
+
+  containerMenu.addEventListener("click", () => {
+    let containerPrincipal = document.getElementById("container__principal");
+    containerPrincipal.removeChild(containerMenu);
+    containerPrincipal.prepend(menuLateral());
+  });
 
   return containerMenu;
 };
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import { getaoSaldo, dynamicallyGenerateInput } from "./pages/gestao__saldo.js";
-import { Menu } from "./components/menu/index.js";
+import { menuHamburguer } from "./components/menu/index.js";
 
 document.getElementById("container__principal").innerHTML = getaoSaldo();
 dynamicallyGenerateInput();
-document.getElementById("container__principal").prepend(Menu());
+document.getElementById("container__principal").prepend(menuHamburguer());
