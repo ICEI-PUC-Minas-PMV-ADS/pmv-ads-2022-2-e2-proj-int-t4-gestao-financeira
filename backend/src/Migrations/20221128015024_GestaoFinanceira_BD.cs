e157f20c900b56eaa11b5f8060ef2076e8e4acf4
Pedro Santos <103754512+pedrosantos111599@users.noreply.github.com>
2022-11-27 22:54:27 -0300

    Atualiza migrations

---
+++ backend/src/Migrations/20221128015024_GestaoFinanceira_BD.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gestão_Financeira.Migrations
+{
+    public partial class GestaoFinanceira_BD : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
